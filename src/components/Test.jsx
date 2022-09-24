38e663d938fcfb5bc51425e3afb1f9b544eaaeb6
Dinh Tri Thuc <dinhtrithuc2001@gmail.com>
2022-09-24 21:01:43 +0700

    first

---
+++ src/components/Test.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+    render() {
+        return (
+            <div>
+                Test nha
+            </div>
+        );
+    }
+}
+
+export default Test;
